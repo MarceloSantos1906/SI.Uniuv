41671c21305da260a81d0db63f586ed6207de852
Marcelo Santos <marcelo_santos1906@hotmail.com>
2024-09-20 16:38:19 -0300

    Add files via upload

---
+++ 7thModule/Programação Web 3/final-project-api/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+    UserModule,
+    CategoryModule,
+    OrderModule,
+    ProductModule,
+  ],
+})
+export class AppModule {}
