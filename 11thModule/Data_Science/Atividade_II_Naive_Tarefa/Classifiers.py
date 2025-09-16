87dbec591c0791b7890b4ee1f9fda4754467fa9c
Marcelo Santos <marcelo_santos1906@hotmail.com>
2025-09-16 11:26:23 -0300

    Update Classifiers.py

--- 11thModule/Data_Science/Atividade_II_Naive_Tarefa/Classifiers.py
+++ 11thModule/Data_Science/Atividade_II_Naive_Tarefa/Classifiers.py
@@ -1,13 +1,14 @@
-from sklearn.metrics import accuracy_score
 from yellowbrick.classifier import ConfusionMatrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.cluster import KMeans
 import pickle
 import os
 
 directory = "./pkl/"
 pickle_data = []
-
+"""
 print("\n\n--- Gaussian Naive Bayes Classifier ---\n\n")
 
 for file in os.listdir(directory):
@@ -50,4 +51,24 @@ for file in os.listdir(directory):
             cm.fit(x_train, y_train)
             cm.score(x_test, y_test)
             print(f"Accuracy for {file}: {accuracy}")
+"""
+print("\n\n--- K-Means Classifier ---\n\n")
+
+for file in os.listdir(directory):
+    if file.endswith(".pkl"):
+        print("Loading file: " + file)
+        with open(os.path.join(directory, file), "rb") as f:
+            unpickler = pickle.Unpickler(f)
+            x_train, y_train, x_test, y_test = unpickler.load()
+            #x_train, y_train, x_test, y_test = pickle.load(f)
+            print(f"x_train shape: {x_train.shape}, y_train shape: {y_train.shape}")
+            print(f"x_test shape: {x_test.shape}, y_test shape: {y_test.shape}")
+            if len(x_train) != len(y_train) or len(x_test) != len(y_test):
+                print(f"Inconsistent number of samples, skipping {file}.\n")
+                continue
+            model = KMeans(n_clusters=18)
+            model.fit(x_train, y_train)
+            previsoes = model.predict(x_test)
+            accuracy = accuracy_score(y_test, previsoes)
+            print(f"Accuracy for {file}: {accuracy}\n")
 
