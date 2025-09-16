14fc7713d65152a0375bd0747d1337c61ed22f20
Marcelo Santos <marcelo_santos1906@hotmail.com>
2025-09-16 11:28:12 -0300

    Create Databases.kt

---
+++ 11thModule/Software Dev 3/Kotlin API/marcelo/src/main/kotlin/Databases.kt
@@ -0,0 +1,113 @@
+package br.com.api
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.Connection
+import java.sql.DriverManager
+import org.jetbrains.exposed.sql.*
+
+fun Application.configureDatabases() {
+    val dbConnection: Connection = connectToPostgres(embedded = true)
+    val cityService = CityService(dbConnection)
+    
+    routing {
+        post("/cities") {
+            val city = call.receive<City>()
+            val id = cityService.create(city)
+            call.respond(HttpStatusCode.Created, id)
+        }
+
+
+
+        get("/cities/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            try {
+                val city = cityService.read(id)
+                call.respond(HttpStatusCode.OK, city)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+
+
+
+        put("/cities/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            val user = call.receive<City>()
+            cityService.update(id, user)
+            call.respond(HttpStatusCode.OK)
+        }
+
+
+
+        delete("/cities/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            cityService.delete(id)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+    val database = Database.connect(
+        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+        user = "root",
+        driver = "org.h2.Driver",
+        password = "",
+    )
+    val userService = UserService(database)
+    routing {
+        post("/users") {
+            val user = call.receive<ExposedUser>()
+            val id = userService.create(user)
+            call.respond(HttpStatusCode.Created, id)
+        }
+
+
+
+        get("/users/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            val user = userService.read(id)
+            if (user != null) {
+                call.respond(HttpStatusCode.OK, user)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+
+
+
+        put("/users/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            val user = call.receive<ExposedUser>()
+            userService.update(id, user)
+            call.respond(HttpStatusCode.OK)
+        }
+
+
+
+        delete("/users/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            userService.delete(id)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
+
+
+
+fun Application.connectToPostgres(embedded: Boolean): Connection {
+    Class.forName("org.postgresql.Driver")
+    if (embedded) {
+        return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
+    } else {
+        val url = environment.config.property("postgres.url").getString()
+        log.info("Connecting to postgres database at $url")
+        val user = environment.config.property("postgres.user").getString()
+        val password = environment.config.property("postgres.password").getString()
+
+        return DriverManager.getConnection(url, user, password)
+    }
+}
